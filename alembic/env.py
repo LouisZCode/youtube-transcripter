93918c3b0f7068a40d17ee4515a76c49149f7134
Luis Zermeno <lgzg90@hotmail.com>
2026-02-15 09:50:16 +0100

    feature: update in database to async Railway and migration with Railway

--- alembic/env.py
+++ alembic/env.py
@@ -14,7 +14,10 @@ from alembic import context
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option("sqlalchemy.url", os.getenv("DATABASE_URL"))
+database_url = os.getenv("DATABASE_URL")
+if database_url and database_url.startswith("postgresql://"):
+    database_url = database_url.replace("postgresql://", "postgresql+asyncpg://", 1)
+config.set_main_option("sqlalchemy.url", database_url)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
